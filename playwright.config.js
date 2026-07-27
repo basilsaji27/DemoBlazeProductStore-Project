8b9c0868b775fe1412039a81d7a419744e4185ec
basilsaji27 <basilsaji88@gmail.com>
2026-07-27 19:45:35 +0530

    Latest Commit

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['allure-playwright']
+    ['allure-playwright'],
+    ['junit',{outputFile:'test-results/results.xml'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
